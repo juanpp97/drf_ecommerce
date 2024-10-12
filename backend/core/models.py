b73bd431e2ef60cad27cb4a4f1d730dfb02f3e15
Juan P. Peñaloza <juanpenaloza97@gmail.com>
2024-10-12 20:29:09 -0300

    Finished models and migrations

--- backend/core/models.py
+++ backend/core/models.py
@@ -2,19 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 
-class ShippingAddress(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=100)
-    country = CountryField(default='AR', blank=False, null=False, verbose_name='país')
-    city = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=20)
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    @classmethod
+    def get_default(cls):
+        category, created = cls.objects.get_or_create(name = "Sin Categoría")
+        return category.pk
+        
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
 
 class Product(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(blank = True, default=0)
-    discount = models.ForeignKey('Discount', on_delete=models.SET_NULL, null=True, blank=True, related_name='product')
+    category = models.ForeignKey('Category', on_delete=models.SET_DEFAULT, default=Category.get_default(), null=True, blank=True, related_name='product')
+    discount = models.ForeignKey('Discount', on_delete=models.SET_NULL, null=True, blank=True, related_name='product_discount')
     active = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
@@ -25,13 +33,10 @@ class Product(models.Model):
 class Images(models.Model):
     path = models.ImageField(upload_to = 'products')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    class Meta:
-        verbose_name_plural = 'Categories'
-        
+    
+    def delete(self):
+        self.path.storage.delete(self.path.name)
+        super().delete()
 
 class Discount(models.Model):
     discount = models.DecimalField(max_digits=10, decimal_places=3)
@@ -42,12 +47,19 @@ class Coupon(models.Model):
     code = models.CharField(max_length=45, unique=True)
     status = models.BooleanField()
 
+class ShippingAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    country = CountryField(default='AR', blank=False, null=False, verbose_name='pais')
+    city = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+    
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     subtotal = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
-    order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='items')
+    order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='order_items')
 
 
     def save(self, *args, **kwargs):
@@ -66,19 +78,29 @@ class Order(models.Model):
     shipping_info = models.ForeignKey(ShippingAddress, on_delete=models.SET_NULL, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     total = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    coupon = models.ForeignKey(Coupon, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     created_at = models.DateTimeField(auto_now_add=True)
+    
 
-
-
-        
 
 class Cart(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     
+    @property
+    def total(self):
+        total = 0
+        for item in self.items.all():
+            total += item.subtotal
+        return total
     
-
-
-
-    
+        
 class CartItem(models.Model):
-    pass
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
+
+
+    def save(self, *args, **kwargs):
+        self.subtotal = self.quantity * self.product.price
+        return super().save(*args, **kwargs)
