815ff7d42f5d6577ac150e61845089e0ae6e10d2
Gabriel <gabrielignacioetchegoyen@hotmail.com>
2024-10-13 19:27:13 -0300

    Product, Category and Images serializers

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Product)
+admin.site.register(models.Category)
+admin.site.register(models.Cart)
+admin.site.register(models.CartItem)
+admin.site.register(models.Order)
+admin.site.register(models.OrderItem)
+admin.site.register(models.Images)
+admin.site.register(models.Discount)
+admin.site.register(models.Coupon)
+admin.site.register(models.ShippingAddress)
+
---
+++ backend/core/serializers.py
@@ -0,0 +1,88 @@
+from rest_framework import serializers
+from .models import Product, Category, Cart, CartItem, Order, OrderItem, Images, Discount, Coupon, ShippingAddress
+
+default_errors = {
+    'required': 'El campo es obligatorio',
+    'blank': 'El campo no puede estar en blanco.',
+    'max_length': 'El campo no puede tener más de {max_length} caracteres.',
+    'invalid': 'El tipo de dato es inválido',
+
+}
+decimals_errors = {        
+    'max_digits': 'No debe exceder de {max_length} digitos.',
+    'max_decimal_places': 'No debe exceder de {max_decimal_places} decimales.',
+}
+
+choice_errors = {
+    'invalid_choice': '{input} no es una opción válida'
+}
+
+key_errors = {
+    'does_not_exist': 'No existe registro con pk = {pk_value}',
+    'incorrect_type': 'Tipo de dato incorrecto',
+}
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+        extra_kwargs = {
+            'name': {
+                'error_messages': default_errors
+            }
+        }
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Images
+        fields = ['path']
+    def to_representation(self, instance):
+        return {
+            'ruta': instance.path.url
+        }
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many = True)
+    class Meta:
+        Model = Product
+        fields = ['id', 'name', 'description', 'price', 'stock', 'category', 'discount', 'created_at', 'update_at']
+        extra_kwargs = {
+            'name': {
+                'error_messages': default_errors,
+            },
+            'description': {
+                'error_messages': default_errors,
+            },
+            'price': {
+                'error_messages': {**default_errors, **decimals_errors}
+            },
+            'stock': {
+                'error_messages': default_errors,
+            },
+            'category': {
+                'error_messages': {**default_errors, **key_errors},
+            },
+            'discount': {
+                'error_messages': {**default_errors, **key_errors},
+            },
+            'created_at': {
+                'error_messages': default_errors,
+                'read_only': True,
+            },
+            'updated_at': {
+                'error_messages': default_errors,
+                'read_only': True,
+            },
+        }
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['category'] = instance.category.name
+        representation['discount'] = f'{instance.discount.discount * 100}%'
+        
+        
+
+
+        
+
